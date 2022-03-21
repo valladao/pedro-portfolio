77e364f1b99de010e202179e0855308c60f16271
Manoel Valladao <valladao@gmail.com>
2022-03-21 20:31:45 -0300

    Create Basic_Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
+import Basic_Layout from '../components/layouts/basic'
 import Base_Template from '../components/templates/base'
 
 const Home: NextPage = () => {
   return (
-    <Base_Template />
+    <Basic_Layout>
+      <Base_Template />
+    </Basic_Layout>
+
   )
 }
 
