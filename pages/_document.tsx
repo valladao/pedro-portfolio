a618405efe3ced41d7540ed3ce66e06e821c3300
Manoel Valladao <valladao@gmail.com>
2022-03-22 21:00:09 -0300

    Add all fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,15 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,400&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Gruppo&family=Open+Sans:ital,wght@0,300;0,400;0,600;1,400&display=swap"
+            rel="stylesheet"
+          />
+          <link href="https://use.typekit.net/vwp7bkc.css" rel="stylesheet" />
         </Head>
         <body>
           <Main />
