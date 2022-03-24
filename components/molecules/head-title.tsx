b5b1efd4533cffca38c3b89f65cfc71712a03375
Manoel Valladao <valladao@gmail.com>
2022-03-23 22:39:39 -0300

    Fix Head_Title CSS classes

--- components/molecules/head-title.tsx
+++ components/molecules/head-title.tsx
@@ -2,14 +2,21 @@ import Header_Line from "../atoms/header-line";
 import Subtitle_1 from "../atoms/subtitle-1";
 import Title_1 from "../atoms/title-1";
 import styles from '../../styles/molecules/Head_Title.module.css'
+import Link from "next/link";
 
 const Head_Title = () => {
   return (
-    <div className={styles.Head_Title}>
-      <Title_1></Title_1>
-      <Header_Line></Header_Line>
-      <Subtitle_1></Subtitle_1>
+
+    <div className={styles.Head_Title + ' head-title'}>
+      <Link href="/">
+        <a>
+          <Title_1></Title_1>
+          <Header_Line></Header_Line>
+          <Subtitle_1></Subtitle_1>
+        </a>
+      </Link>
     </div>
+
    );
 };
 
