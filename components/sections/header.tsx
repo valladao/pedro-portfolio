19ea7e7ef5603fcad0b59d72a3ca2f3e78f53e95
Manoel Valladao <valladao@gmail.com>
2022-03-26 15:31:23 -0300

    Add Header styles

--- components/sections/header.tsx
+++ components/sections/header.tsx
@@ -1,10 +1,11 @@
 import Head_Title from "../molecules/head-title"
 import Menu_Bar from "../molecules/menu-bar"
 import Menu from "../molecules/menu"
+import styles from "../../styles/organisms/Header.module.css"
 
 const Header = () => {
   return (
-    <div>
+    <div className={styles.Header + ' header'}>
       <Head_Title></Head_Title>
       <Menu_Bar></Menu_Bar>
       <Menu></Menu>
