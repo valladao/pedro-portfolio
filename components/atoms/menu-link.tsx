c52091c2538f3f39891ea4c8ba9426e8a8b102ed
Manoel Valladao <valladao@gmail.com>
2022-03-26 12:09:58 -0300

    Add Menu_Link

---
+++ components/atoms/menu-link.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/atoms/Menu_Link.module.css'
+
+const Menu_Link = () => {
+  return (
+    <li className={styles.Menu_Link}><a href="#">About</a></li>
+  );
+};
+
+export default Menu_Link;
