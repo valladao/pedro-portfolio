c2698250dee1717d9f42bdaacc9429554d0f01b8
Manoel Valladao <valladao@gmail.com>
2022-03-30 22:01:14 -0300

    Add Page_Title

---
+++ src/components/atoms/page-title.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/atoms/Page_Title.module.css'
+
+const Page_Title = () => {
+  return (
+    <h1 className={styles.Page_Title}>Portfolio</h1>
+  );
+};
+
+export default Page_Title
