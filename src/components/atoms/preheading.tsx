7cc799ba31dc61a64a6798569d60165a74762f00
Manoel Valladao <valladao@gmail.com>
2022-04-05 06:33:00 -0300

    Add Preheading

---
+++ src/components/atoms/preheading.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/atoms/Preheading.module.css'
+
+const Preheading = () => {
+  return (
+    <p className={styles.Preheading}>Brazilian concert, film & media composer and pianist, currently living in London.</p>
+  );
+};
+
+export default Preheading;
