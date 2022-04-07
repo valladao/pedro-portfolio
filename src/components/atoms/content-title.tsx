0cdb42da3a07837b184290d2eabea150a0e64fd7
Manoel Valladao <valladao@gmail.com>
2022-04-07 20:16:39 -0300

    Add Content_Title

---
+++ src/components/atoms/content-title.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/atoms/Content_Title.module.css'
+
+const Content_Title = () => {
+  return (
+    <h1 className={styles.Content_Title}>Biography</h1>
+  );
+};
+
+export default Content_Title
