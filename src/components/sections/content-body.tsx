dddd4e75379b1dc456fe0166c11260abac41e397
Manoel Valladao <valladao@gmail.com>
2022-04-09 18:22:21 -0300

    Add dynamic (Portable) text to Content_Body

--- src/components/sections/content-body.tsx
+++ src/components/sections/content-body.tsx
@@ -1,14 +1,19 @@
 import styles from '../../styles/sections/Content_Body.module.css'
-import Paragraph from '../atoms/paragraph';
+import Paragraph from '../atoms/paragraph'
+import { PortableText } from '@portabletext/react'
 
-const Content_Body = () => {
+type Content_Body_Props = {
+  portableText: Array<Block>
+}
+
+const Content_Body = ({ portableText }: Content_Body_Props) => {
   return (
     <div className={styles.Content_Body}>
-      <Paragraph></Paragraph>
-      <Paragraph></Paragraph>
-      <Paragraph></Paragraph>
+      <PortableText
+        value={portableText}
+      ></PortableText>
     </div>
-  );
-};
+  )
+}
 
-export default Content_Body;
+export default Content_Body
--- src/scripts/get-base-pages-query.ts
+++ src/scripts/get-base-pages-query.ts
@@ -2,7 +2,7 @@ import client from "../client"
 
 export default async function getBasePagesQuery(slug: string) {
   const query = await client.fetch(
-    `*[_type == "base-pages" && slug == "${slug}"]{slug,pageTitle,imageDesktop,imageMobile,altText,email}`
+    `*[_type == "base-pages" && slug == "${slug}"]{slug,pageTitle,imageDesktop,imageMobile,altText,email,content}`
   )
   return query[0]
 }
