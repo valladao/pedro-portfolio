e8432bb89d632411928edb587d98e94d8e7917cb
Manoel Valladao <valladao@gmail.com>
2022-04-10 17:22:14 -0300

    Add Subtitle_2

---
+++ src/components/atoms/subtitle-2.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/atoms/Subtitle_2.module.css'
+
+const Subtitle_2 = () => {
+  return (
+    <h2 className={styles.Subtitle_2}>Recent Works & Releases</h2>
+  )
+}
+
+export default Subtitle_2
