fdbf194ea71af3371891e22ca3e3891e9d7794b7
Manoel Valladao <valladao@gmail.com>
2022-04-10 18:07:17 -0300

    Add Album_Cover

---
+++ src/components/atoms/album-cover.tsx
@@ -0,0 +1,11 @@
+const Album_Cover = () => {
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src="https://i.ibb.co/T2Yk6sp/album-cover.jpg"
+      alt=""
+    />
+  )
+}
+
+export default Album_Cover
