9ebd8406a516e42d2046ce0d5df697a007e6c5ef
Manoel Valladao <valladao@gmail.com>
2022-04-10 18:20:29 -0300

    Add Album_Title_2

---
+++ src/components/atoms/album-title-2.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/atoms/Album_Title_2.module.css'
+
+const Album_Title_2 = () => {
+  return (
+    <h3 className={styles.Album_Title_2}>
+      Symphony in two movements in E/E-flat minor, Op.3
+    </h3>
+  );
+};
+
+export default Album_Title_2;
