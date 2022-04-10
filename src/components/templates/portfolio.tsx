8063a6a8c02a6c55d411e98d42ea98d79d80965a
Manoel Valladao <valladao@gmail.com>
2022-04-10 14:03:28 -0300

    Add dynamic section rendering

--- src/components/templates/portfolio.tsx
+++ src/components/templates/portfolio.tsx
@@ -6,11 +6,34 @@ type Portfolio_Template_Props = {
   data: Portfolio_Pages_Props
 }
 
+const sectionRender = (section: heroImage) => {
+
+  switch (section._type) {
+    case "heroImage":
+      return <Hero_Image key={section._key} imageDesktop={section.imageDesktop} imageMobile={section.imageMobile} altText={section.altText}></Hero_Image>
+
+    default:
+      break;
+  }
+}
+
+const sectionLoop = (sections: Array<heroImage>) => {
+  let renderedSections: Array<React.ReactNode> = []
+
+  for (const section of sections) {
+    renderedSections.push(sectionRender(section))
+  }
+
+  return renderedSections
+}
+
 const Portfolio_Template = ({ data }: Portfolio_Template_Props) => {
   return (
     <>
       <Title_Bar pageTitle="Portfolio"></Title_Bar>
-      <Hero_Image imageDesktop={data.sections[0].imageDesktop} imageMobile={data.sections[0].imageMobile} altText={data.sections[0].altText}></Hero_Image>
+      {sectionLoop(data.sections).map(
+        (component) => { return component }
+      )}
     </>
   )
 }
