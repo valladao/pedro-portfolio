2ac90efde3f806bf40cd67a09fbc675134654e34
Manoel Valladao <valladao@gmail.com>
2022-04-10 17:52:40 -0300

    Add Link_Button_2

---
+++ src/components/atoms/link-button-2.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/atoms/Link_Button_2.module.css'
+
+const Link_Button_2 = () => {
+  return (
+    <div className={styles.Link_Button_2}>
+      <h4>More Info</h4>
+    </div>
+  )
+}
+
+export default Link_Button_2
