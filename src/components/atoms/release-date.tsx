dff8e7ee1e13a7b82de3f5b87899d4ae9b52c9af
Manoel Valladao <valladao@gmail.com>
2022-04-10 18:01:33 -0300

    Add Release_Date

---
+++ src/components/atoms/release-date.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/atoms/Release_Date.module.css'
+
+const Release_Date = () => {
+  return (
+    <h3 className={styles.Release_Date}>2022</h3>
+  )
+}
+
+export default Release_Date
