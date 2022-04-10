ddf57aa9bb262874bf266f51f5f6cffa888f4a58
Manoel Valladao <valladao@gmail.com>
2022-04-10 18:16:50 -0300

    Add Album_Title

---
+++ src/components/atoms/album-title.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/atoms/Album_Title.module.css'
+
+const Album_Title = () => {
+  return (
+    <h2 className={styles.Album_Title}>Symphony, Opus 3</h2>
+  )
+}
+
+export default Album_Title
