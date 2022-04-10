48694bc4c83cc9c731b3007dceed4cdfea14118a
Manoel Valladao <valladao@gmail.com>
2022-04-10 18:08:20 -0300

    Add Album_Cover

--- src/components/atoms/album-cover.tsx
+++ src/components/atoms/album-cover.tsx
@@ -1,7 +1,10 @@
+import styles from '../../styles/atoms/Album_Cover.module.css'
+
 const Album_Cover = () => {
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
+      className={styles.Album_Cover}
       src="https://i.ibb.co/T2Yk6sp/album-cover.jpg"
       alt=""
     />
