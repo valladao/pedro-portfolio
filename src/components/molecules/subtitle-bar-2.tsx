ebf5f5b444ccced545f3ce2f0fc46bf7203ba4d9
Manoel Valladao <valladao@gmail.com>
2022-04-10 18:31:40 -0300

    Add Subtitle_Bar_2

---
+++ src/components/molecules/subtitle-bar-2.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/atoms/Subtitle_Bar_2.module.css'
+import Subtitle_2 from '../atoms/subtitle-2'
+
+const Subtitle_Bar_2 = () => {
+  return (
+    <div className={styles.Subtitle_Bar_2 + ' subtitle-bar-2'}>
+      <Subtitle_2></Subtitle_2>
+    </div>
+  )
+}
+
+export default Subtitle_Bar_2
