d852018da5bc8e9bbc534f447314e7c93b81e650
Manoel Valladao <valladao@gmail.com>
2022-04-21 08:37:14 -0300

    Add SoundCloud_Track

---
+++ src/components/molecules/soundcloud-track.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/molecules/SoundCloud_Track.module.css'
+import SoundCloud from '../atoms/soundcloud';
+
+const SoundCloud_Track = () => {
+  return (
+    <div className={styles.SoundCloud_Track}>
+      <SoundCloud></SoundCloud>
+    </div>
+  )
+}
+
+export default SoundCloud_Track
