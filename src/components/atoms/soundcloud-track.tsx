7aa50215ccdd5f3715c2a66b666987aefa5f6610
Manoel Valladao <valladao@gmail.com>
2022-04-23 11:21:15 -0300

    Add trackId to SoundCloud

--- src/components/atoms/soundcloud-track.tsx
+++ src/components/atoms/soundcloud-track.tsx
@@ -1,6 +1,13 @@
 import styles from '../../styles/atoms/SoundCloud_Track.module.css'
 
-const SoundCloud = () => {
+type SoundCloud_Props = {
+  trackId: string
+}
+
+const SoundCloud = ({ trackId }: SoundCloud_Props) => {
+
+  const trackUrl = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${trackId}&color=%230051cc&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true&download=false`
+
   return (
     <div className={styles.SoundCloud_Track}>
       <iframe
@@ -8,7 +15,7 @@ const SoundCloud = () => {
         scrolling="no"
         frameBorder="no"
         allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/142174239&color=%230051cc&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true&download=false"
+        src={trackUrl}
       ></iframe>
     </div>
   )
--- src/components/sections/track-list.tsx
+++ src/components/sections/track-list.tsx
@@ -7,12 +7,12 @@ const Track_List = () => {
     <>
       <Subtitle_Bar_2 text="Selected Listenings"></Subtitle_Bar_2>
       <div className={styles.Track_List + ' track-list'}>
-        <SoundCloud_Track></SoundCloud_Track>
-        <SoundCloud_Track></SoundCloud_Track>
-        <SoundCloud_Track></SoundCloud_Track>
-        <SoundCloud_Track></SoundCloud_Track>
-        <SoundCloud_Track></SoundCloud_Track>
-        <SoundCloud_Track></SoundCloud_Track>
+        <SoundCloud_Track trackId="1255510120"></SoundCloud_Track>
+        <SoundCloud_Track trackId="142174239"></SoundCloud_Track>
+        <SoundCloud_Track trackId="143095171"></SoundCloud_Track>
+        <SoundCloud_Track trackId="258302465"></SoundCloud_Track>
+        <SoundCloud_Track trackId="257228278"></SoundCloud_Track>
+        <SoundCloud_Track trackId="24880470"></SoundCloud_Track>
       </div>
     </>
   )
