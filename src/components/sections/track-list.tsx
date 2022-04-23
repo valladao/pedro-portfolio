dab21f2a4e507ddb45a355f1d60101ee24e2beee
Manoel Valladao <valladao@gmail.com>
2022-04-23 07:22:55 -0300

    Add Subtitle_Bar_2 to Track_List

--- src/components/sections/track-list.tsx
+++ src/components/sections/track-list.tsx
@@ -1,16 +1,21 @@
 import styles from '../../styles/sections/Track_List.module.css'
 import SoundCloud_Track from '../atoms/soundcloud-track';
+import Subtitle_2 from '../atoms/subtitle-2';
+import Subtitle_Bar_2 from '../molecules/subtitle-bar-2';
 
 const Track_List = () => {
   return (
-    <div className={styles.Track_List + ' track-list'}>
-      <SoundCloud_Track></SoundCloud_Track>
-      <SoundCloud_Track></SoundCloud_Track>
-      <SoundCloud_Track></SoundCloud_Track>
-      <SoundCloud_Track></SoundCloud_Track>
-      <SoundCloud_Track></SoundCloud_Track>
-      <SoundCloud_Track></SoundCloud_Track>
-    </div>
+    <>
+      <Subtitle_Bar_2></Subtitle_Bar_2>
+      <div className={styles.Track_List + ' track-list'}>
+        <SoundCloud_Track></SoundCloud_Track>
+        <SoundCloud_Track></SoundCloud_Track>
+        <SoundCloud_Track></SoundCloud_Track>
+        <SoundCloud_Track></SoundCloud_Track>
+        <SoundCloud_Track></SoundCloud_Track>
+        <SoundCloud_Track></SoundCloud_Track>
+      </div>
+    </>
   );
 };
 
