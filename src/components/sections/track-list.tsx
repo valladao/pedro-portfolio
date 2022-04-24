b1a419fad9c6e5ad1289211c2dc6ad8af39188ea
Manoel Valladao <valladao@gmail.com>
2022-04-24 13:26:58 -0300

    Add Spotify_Track and YouTube_Track to Track_List

--- src/components/sections/track-list.tsx
+++ src/components/sections/track-list.tsx
@@ -1,5 +1,7 @@
 import styles from '../../styles/sections/Track_List.module.css'
 import SoundCloud_Track from '../atoms/soundcloud-track'
+import Spotify_Track from '../atoms/spotify-track'
+import YouTube_Track from '../atoms/youtube-track'
 import Subtitle_Bar_2 from '../molecules/subtitle-bar-2'
 
 type Track_List_Props = {
@@ -12,9 +14,9 @@ const Track_List = ({ tracks }: Track_List_Props) => {
       <Subtitle_Bar_2 text="Selected Listenings"></Subtitle_Bar_2>
       <div className={styles.Track_List + ' track-list'}>
         <SoundCloud_Track trackId={tracks[0].trackID}></SoundCloud_Track>
-        <SoundCloud_Track trackId={tracks[1].trackID}></SoundCloud_Track>
+        <Spotify_Track></Spotify_Track>
         <SoundCloud_Track trackId={tracks[2].trackID}></SoundCloud_Track>
-        <SoundCloud_Track trackId={tracks[3].trackID}></SoundCloud_Track>
+        <YouTube_Track></YouTube_Track>
         <SoundCloud_Track trackId={tracks[4].trackID}></SoundCloud_Track>
         <SoundCloud_Track trackId={tracks[5].trackID}></SoundCloud_Track>
       </div>
