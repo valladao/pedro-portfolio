49105bbd7bb15734e7035e53434c19acfb3bf9f1
Manoel Valladao <valladao@gmail.com>
2022-04-28 12:05:50 -0300

    Add Album_List to portfolio page

--- src/components/templates/portfolio.tsx
+++ src/components/templates/portfolio.tsx
@@ -1,6 +1,7 @@
 
 import Title_Bar from "../sections/title-bar"
 import sectionsRender from "../../scripts/sectionsRender"
+import Album_List from "../sections/album-list"
 
 type Portfolio_Template_Props = {
   data: Portfolio_Pages_Props
@@ -13,6 +14,7 @@ const Portfolio_Template = ({ data }: Portfolio_Template_Props) => {
       {sectionsRender(data.sections).map(
         (component) => { return component }
       )}
+      <Album_List></Album_List>
     </>
   )
 }
