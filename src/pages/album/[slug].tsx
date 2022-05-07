5daf6850ccf38e2054d99d91ce04da123f18dfa7
Manoel Valladao <valladao@gmail.com>
2022-05-07 15:11:54 -0300

    Album Page initial release

---
+++ src/pages/album/[slug].tsx
@@ -0,0 +1,39 @@
+import Basic_Layout from "../../components/layouts/basic"
+import Portfolio_Template from "../../components/templates/portfolio"
+import getSanityPagesQuery from "../../scripts/get-sanity-pages-query"
+
+type Album_Props = {
+  query: Portfolio_Pages_Props
+}
+
+const Album = ({ query }: Album_Props) => {
+  return (
+    <Basic_Layout>
+      <Portfolio_Template data={query}></Portfolio_Template>
+    </Basic_Layout>
+  )
+}
+
+export async function getServerSideProps() {
+
+  const fields = `
+  {
+    pageTitle,
+    'slug': slug.current,
+    sections[]{
+      ...,
+      album != NULL => { album ->},
+      selectedAlbuns[0] == "all" => { "albunsList": * [_type == "album"] | order(releaseDate desc) | order(_createdAt asc) }
+    }
+  }`
+
+  const query = await getSanityPagesQuery("portfolio", "portfolio", fields)
+
+  return {
+    props: {
+      query
+    }
+  }
+}
+
+export default Album
