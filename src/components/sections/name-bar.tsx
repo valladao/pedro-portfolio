5c0109a6426b13a4809ad2b46fb3da488c7e8911
Manoel Valladao <valladao@gmail.com>
2023-03-04 16:44:08 -0300

    Improve Full Name and Name Bar to make it reusable

--- src/components/sections/name-bar.tsx
+++ src/components/sections/name-bar.tsx
@@ -1,13 +1,13 @@
 import styles from '../../styles/sections/Name_Bar.module.css'
-import Full_Name from '../atoms/full-name';
+import Full_Name from '../atoms/full-name'
 
-const Name_Bar = () => {
+const Name_Bar = ({fullName, shortName}: {fullName: string, shortName: string}) => {
   return (
     <div className={styles.Name_Bar + ' name-bar'}>
-      <Full_Name fullName="Pedro Henrique Valladao"></Full_Name>
-      <Full_Name fullName="Pedro Henrique dos Santos Valladao"></Full_Name>
+      <Full_Name name={shortName}></Full_Name>
+      <Full_Name name={fullName}></Full_Name>
     </div>
   );
 };
 
-export default Name_Bar;
+export default Name_Bar
