f21cdf6e2555f9c4ec643edbb100083008bcafd5
Manoel Valladao <valladao@gmail.com>
2023-03-04 19:43:18 -0300

    Add text section

--- src/components/sections/album-hero-banner.tsx
+++ src/components/sections/album-hero-banner.tsx
@@ -8,11 +8,14 @@ const backgroundImage = {
 
 const Album_Hero_Banner = ({albumTitle, shortTitle, albumCover, altText}: Album_Hero_Banner) => {
   return (
-    <div className="album-hero-banner">
-      <div className={styles.Album_Hero_Banner} style={backgroundImage}>
+    <div className={styles.Album_Hero_Banner + ' album-hero-banner'}>
+      <div style={backgroundImage}>
         <Album_Tile albumTitle={albumTitle} albumCover={albumCover} altText={altText}></Album_Tile>
       </div>
       <Name_Bar fullName={albumTitle} shortName={shortTitle}></Name_Bar>
+      <div>
+        <p>These 5 Morceaux (Pieces) for piano are exquisite for their delicacy, imagination, and fantasy. Each one of them transports the listener to a world of dreams and nostalgia, at times playful and exciting but also thoughtful and mellow. They were composed with the pretext of always developing the very first musical thought or melody that arrived at the composer’s hands on the piano. So, they are quite different from each other, and unique themselves. They constituted then compositional exercises but that, regardless of this nature, became great pieces of music.</p>
+      </div>
     </div>
   )
 }
