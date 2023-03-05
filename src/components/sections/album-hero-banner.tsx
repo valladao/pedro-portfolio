0491f6a3cc312a50b600d08f22172e4204c24506
Manoel Valladao <valladao@gmail.com>
2023-03-04 22:00:24 -0300

    Add desktop styles

--- src/components/sections/album-hero-banner.tsx
+++ src/components/sections/album-hero-banner.tsx
@@ -10,10 +10,13 @@ const Album_Hero_Banner = ({albumTitle, shortTitle, albumCover, altText}: Album_
   return (
     <div className={styles.Album_Hero_Banner + ' album-hero-banner'}>
       <div style={backgroundImage}>
+        <div className={styles.Album_Hero_Banner_Text}>
+          <p>These 5 Morceaux (Pieces) for piano are exquisite for their delicacy, imagination, and fantasy. Each one of them transports the listener to a world of dreams and nostalgia, at times playful and exciting but also thoughtful and mellow. They were composed with the pretext of always developing the very first musical thought or melody that arrived at the composer’s hands on the piano. So, they are quite different from each other, and unique themselves. They constituted then compositional exercises but that, regardless of this nature, became great pieces of music.</p>
+        </div>
         <Album_Tile albumTitle={albumTitle} albumCover={albumCover} altText={altText}></Album_Tile>
       </div>
       <Name_Bar fullName={albumTitle} shortName={shortTitle}></Name_Bar>
-      <div>
+      <div className={styles.Album_Hero_Banner_Text}>
         <p>These 5 Morceaux (Pieces) for piano are exquisite for their delicacy, imagination, and fantasy. Each one of them transports the listener to a world of dreams and nostalgia, at times playful and exciting but also thoughtful and mellow. They were composed with the pretext of always developing the very first musical thought or melody that arrived at the composer’s hands on the piano. So, they are quite different from each other, and unique themselves. They constituted then compositional exercises but that, regardless of this nature, became great pieces of music.</p>
       </div>
     </div>
