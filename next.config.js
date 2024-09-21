d1a4c7c6e8674183dc178583f7aa9d7af2bcfcaa
Manoel Valladão <valladao@gmail.com>
2024-09-21 18:01:38 -0300

    Feature: Migrate Yarn to NPM (#38)
    * Removed yarn, updated packages and npm run dev worked
    
    * Fixed <a> error
    
    * Removed sanityClient warning
    
    * Update domains to remotePatterns
    
    * Fix image issue to make it square
    
    * Additional updates to attend vercel
    
    * legacy-peer-deps

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.sanity.io"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io", // Sanity's CDN
+        port: "", // no specific port needed
+        pathname: "/**" // wildcard for all paths
+      }
+    ]
   }
 }
 
